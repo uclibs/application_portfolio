e6fc989d49d788070ac2b2663e0b038bff92b3eb
Uzval-Mallepeddi <uzval007@gmail.com>
2019-11-15 13:40:52 -0500

    Install and Configure User Abilities

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_191_018_163_508) do
+ActiveRecord::Schema.define(version: 20_191_115_183_904) do
   create_table 'users', force: :cascade do |t|
     t.string 'email', default: '', null: false
     t.string 'encrypted_password', default: '', null: false
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20_191_018_163_508) do
     t.datetime 'remember_created_at'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.string 'roles'
     t.index ['email'], name: 'index_users_on_email', unique: true
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
