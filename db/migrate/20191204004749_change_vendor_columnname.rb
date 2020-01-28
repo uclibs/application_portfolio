404a9d6c98f2a4b5abbdbcd9b34d86988d6444b5
Uzval Mallepeddi <uzval007@gmail.com>
2020-01-28 14:20:56 -0500

    Minor Fixes (#76)
    * Minor Fixes
    
    * Minor Fixes

--- db/migrate/20191204004749_change_vendor_columnname.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# Change vendor_records column name
-class ChangeVendorColumnname < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :vendor_records, :name, :title
-  end
-end
