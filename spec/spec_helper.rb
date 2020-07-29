0564c962ae4dc864b4539252e99d55aeb71587e7
Glen Horton <hortongn@ucmail.uc.edu>
2020-07-29 16:09:55 -0400

    coveralls test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,10 +18,10 @@
 require 'simplecov'
 require 'coveralls'
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
-    [
-          SimpleCov::Formatter::HTMLFormatter,
-              Coveralls::SimpleCov::Formatter
-                ]
+  [
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter
+  ]
 )
 SimpleCov.start('rails')
 
