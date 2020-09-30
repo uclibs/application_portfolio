e4aad01c5a3b6251ba40dd6e9425e37f44844966
Uzval Mallepeddi <uzval.mallepeddi@gmail.com>
2020-09-30 16:26:29 -0400

    Rubcop corrections

--- db/migrate/20200930184802_remove_status_field.rb
+++ db/migrate/20200930184802_remove_status_field.rb
@@ -3,6 +3,6 @@
 # Remove column in software_records
 class RemoveStatusField < ActiveRecord::Migration[5.2]
   def change
-  	remove_column :software_records, :status, :string
+    remove_column :software_records, :status, :string
   end
 end
