1c2c01c07348b759b27f8b2e397dbf34ceae56d9
Uzval Mallepeddi <uzval007@gmail.com>
2020-10-16 14:32:54 -0400

    Improve Coveralls Coverage (#194)
    * Clean up Status Field
    
    * Remove status field from software records table
    
    * Rubcop corrections
    
    * Import Data script update
    
    * Improve Coveralls Coverage
    
    Co-authored-by: Uzval Mallepeddi <uzval.mallepeddi@gmail.com>

--- spec/controllers/front_controller_spec.rb
+++ spec/controllers/front_controller_spec.rb
@@ -71,4 +71,56 @@ describe FrontController do
       expect(response).to render_template('profile')
     end
   end
+
+  describe '#search' do
+    before do
+      admin = FactoryBot.create(:admin)
+      sign_in_user(admin)
+
+      VendorRecord.create!(
+        title: 'Vendor 1',
+        description: 'test vendor'
+      )
+      SoftwareType.create!(
+        title: 'Web app',
+        description: 'test software type'
+      )
+      Status.create!(
+        title: 'Test',
+        status_type: 'Design'
+      )
+    end
+
+    let(:valid_session) { {} }
+
+    let(:valid_attributes) do
+      {
+        title: 'A Good Software',
+        description: 'A Good description about the software',
+        status_id: Status.first.id,
+        software_type_id: SoftwareType.first.id,
+        vendor_record_id: VendorRecord.first.id,
+        created_by: 'Test Admin',
+        developers: %w[Tester Random],
+        tech_leads: ['Lead 1'],
+        product_owners: %w[Owner1 Owner2]
+      }
+    end
+
+    def sign_in_user(admin)
+      sign_in admin
+    end
+
+    it 'renders the correct search data' do
+      SoftwareRecord.create! valid_attributes
+      get :search, params: { search: 'good' }, session: valid_session
+      expect(response.body).to have_content('A Good Software')
+    end
+
+    it 'redirects to root_path when search param is blank' do
+      SoftwareRecord.create! valid_attributes
+      get :search, params: { search: '' }, session: valid_session
+      expect(response).to redirect_to(root_path)
+    end
+  end
 end
