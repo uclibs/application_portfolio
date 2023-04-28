e571ef3f3d3580ea5c0a287805fad9ff6dbdaa59
Sean Crowe <1069588+crowesn@users.noreply.github.com>
2023-04-28 10:56:34 -0400

    fix software records graph (#290)
    Co-authored-by: crowesn <crowesn@gmail.com>

--- spec/factories/software_records.rb
+++ spec/factories/software_records.rb
@@ -6,8 +6,9 @@ FactoryBot.define do
     description { 'MyText' }
     status_id { Status.first.id }
     date_implemented { '2020-12-12' }
-    vendor_record_id { VendorRecord.second.id }
-    software_type_id { SoftwareType.second.id }
+    vendor_record_id { VendorRecord.first.id }
+    software_type_id { SoftwareType.first.id }
+    hosting_environment_id { HostingEnvironment.first.id }
     created_by { 'Test User' }
   end
 end
