3101f239b89629e952754740e8f85446b831c6d6
Thomas Scherz <scherztc@ucmail.uc.edu>
2023-05-04 12:43:21 -0400

    Adds ChangeRequest Model.

---
+++ app/models/change_request.rb
@@ -0,0 +1,3 @@
+class ChangeRequest < ApplicationRecord
+  belongs_to :software_record
+end
---
+++ spec/factories/change_requests.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :change_request do
+    software_record { nil }
+  end
+end
