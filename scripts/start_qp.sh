e752a22459c4de58ded525972bfc7cba77c3da6a
crowesn <crowesn@gmail.com>
2023-05-16 13:14:41 -0400

    add linux platform to lockfile

--- scripts/start_qp.sh
+++ scripts/start_qp.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo $BASH_SOURCE
+
 if [ ! -f "$(dirname "$(dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" )" )/static/.env.production" ]; then
     echo "Missing updated .env.production file in the static directory. The server may not function properly"
 else
@@ -12,10 +14,11 @@ export RAILS_ENV=production
 # Check if puma is started
 if ! ( [ -f /opt/webapps/application_portfolio/current/tmp/puma/pid ] && pgrep -F /opt/webapps/application_portfolio/current/tmp/puma/pid > /dev/null )
 then
-    bin/bundle exec puma -d
+    #bin/bundle exec puma
+    sudo systemctl start puma-appport.service
 else
-    bin/bundle exec pumactl stop
-    sleep 10
-    cd /opt/webapps/application_portfolio/current
-    bin/bundle exec puma -d
+    sudo systemctl restart puma-appport.service
+   # sleep 10
+   # cd /opt/webapps/application_portfolio/current
+   # bin/bundle exec puma -d
 fi
