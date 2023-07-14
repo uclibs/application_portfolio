0190ed2d05ce9112b4adcf34093f40ed2ec741ca
crowesn <crowesn@gmail.com>
2023-07-14 14:17:19 -0400

    use puma as a service

--- scripts/start_qp.sh
+++ scripts/start_qp.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ ! -f "$(dirname "$(dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" )" )/static/.env.production" ]; then
+if [ ! -f "$(dirname "$(dirname "$(dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" )" )" )/static/.env.production" ]; then
     echo "Missing updated .env.production file in the static directory. The server may not function properly"
 else
-    cp "$(dirname "$(dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" )" )/static/.env.production" "$(dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" )"
+    cp "$(dirname "$(dirname "$(dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" )" )" )/static/.env.production" "$(dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" )"
 fi
 
 export RAILS_RELATIVE_URL_ROOT=/application_portfolio
@@ -12,10 +12,7 @@ export RAILS_ENV=production
 # Check if puma is started
 if ! ( [ -f /opt/webapps/application_portfolio/current/tmp/puma/pid ] && pgrep -F /opt/webapps/application_portfolio/current/tmp/puma/pid > /dev/null )
 then
-    bin/bundle exec puma -d
+    sudo systemctl start puma-appport.service
 else
-    bin/bundle exec pumactl stop
-    sleep 10
-    cd /opt/webapps/application_portfolio/current
-    bin/bundle exec puma -d
+    sudo systemctl restart puma-appport.service
 fi
