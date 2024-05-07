368e9e77b1d7f7f9bc2d7e1b1bc00b1ef1b25c24
Thomas Scherz <scherztc@ucmail.uc.edu>
2024-05-07 12:02:48 -0400

    Fixes null values in export function.

--- spec/exports/software_records_spec.rb
+++ spec/exports/software_records_spec.rb
@@ -1,8 +1,48 @@
 # frozen_string_literal: true
 
 require "#{Dir.pwd}/exports/software_records.rb"
+require 'rails_helper'
+require 'csv'
 
 RSpec.describe SoftwareRecords do
-  describe 'software_records' do
+  describe '#software_records' do
+    let(:software_records) { SoftwareRecords.new }
+
+    before do
+      FactoryBot.create(:software_record, tech_leads: nil, departments: nil, developers: nil,
+                                          product_owners: nil, admin_users: nil)
+    end
+
+    it 'exports CSV with appropriate headers' do
+      csv_content = File.read("#{Dir.pwd}/public/software_records.csv")
+      headers = [
+        'Software Record', 'Description', 'Status', 'Created on', 'Software Type',
+        'Authentication Type', 'Vendor Record', 'Departments', 'Date Implemented',
+        'Date of upgrade', 'Developers', 'Tech Leads', 'Product Owners',
+        'Admin Users', 'Languages Used', 'Production URL', 'Source Code URL',
+        'User Seats', 'Annual Fees', 'Support Contract', 'Hosting Environment',
+        'Current Version', 'Notes', 'Business Value', 'IT Quality', 'Created By',
+        'Requires Change Management review?', 'Last Security Scan',
+        'Last Accessibility Scan', 'Last OGC Review', 'Last Infosec Review',
+        'CM Stakeholders', 'CM Other Notes', 'QA URL', 'Dev_URL', 'Prod_URL',
+        'Production Support Servers', 'Last Record Change', 'Track Uptime',
+        'Monitor Health', 'Monitor Errors'
+      ]
+
+      expect(CSV.parse(csv_content, headers: true).headers).to eq(headers)
+    end
+
+    it 'includes nil attributes as empty fields' do
+      csv_content = File.read("#{Dir.pwd}/public/software_records.csv")
+      csv_rows = CSV.parse(csv_content, headers: true)
+
+      csv_rows.each do |row|
+        expect(row['Tech Leads']).to eq('')
+        expect(row['Departments']).to eq('')
+        expect(row['Developers']).to eq('')
+        expect(row['Product Owners']).to eq('')
+        expect(row['Admin Users']).to eq('')
+      end
+    end
   end
 end
--- spec/factories/software_records.rb
+++ spec/factories/software_records.rb
@@ -14,5 +14,32 @@ FactoryBot.define do
     software_type_id { SoftwareType.first.id }
     hosting_environment_id { HostingEnvironment.first.id }
     created_by { 'Test User' }
+    authentication_type { 'Sample Authentication Type' }
+    date_of_upgrade { Date.today }
+    languages_used { 'Ruby, Python' }
+    production_url { 'http://example.com' }
+    source_code_url { 'http://github.com' }
+    user_seats { 10 }
+    annual_fees { 1000.0 }
+    support_contract { 'Standard Support' }
+    current_version { '1.0.0' }
+    notes { 'Sample notes' }
+    business_value { 'High' }
+    it_quality { 'Good' }
+    requires_cm { true }
+    last_security_scan { Date.today }
+    last_accessibility_scan { Date.today }
+    last_ogc_review { Date.today }
+    last_info_sec_review { Date.today }
+    cm_stakeholders { 'Stakeholder 1' }
+    cm_other_notes { 'No additional notes' }
+    qa_url { 'http://qa.example.com' }
+    dev_url { 'http://dev.example.com' }
+    prod_url { 'http://prod.example.com' }
+    production_support_servers { 'Server 1, Server 2' }
+    last_record_change { Date.today }
+    track_uptime { true }
+    monitor_health { true }
+    monitor_errors { true }
   end
 end
