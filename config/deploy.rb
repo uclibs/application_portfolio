3917c108607b5827beca0e8100f62020c238e747
Thomas Scherz <scherztc@ucmail.uc.edu>
2025-03-05 12:53:24 -0500

    Update Capfile.

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 require 'byebug'
 
 # config valid for current version and patch releases of Capistrano
-lock '~> 3.17.1'
+lock '~> 3.19.2'
 
 set :application, 'application_portfolio'
 set :repo_url, 'https://github.com/uclibs/application_portfolio.git'
