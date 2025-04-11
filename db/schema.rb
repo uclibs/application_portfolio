82f36430b2312836ab9c81cdc3f9dcde634b3569
Janell-Huyck <huyckjl@ucmail.uc.edu>
2025-04-11 15:19:14 -0400

    Add clarification comment to sw.js

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 20_250_411_175_006) do
+ActiveRecord::Schema[7.2].define(version: 2025_04_11_175006) do
   create_table "change_requests", force: :cascade do |t|
     t.integer "software_record_id", null: false
     t.string "change_title"
