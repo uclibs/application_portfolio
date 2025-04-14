2527596d0c281fcd9d4d815bd848a4f805d48d32
Thomas Scherz <scherztc@ucmail.uc.edu>
2025-04-14 14:06:42 -0400

    Removes presence requirement for fields in Change Request.

--- spec/models/change_request_spec.rb
+++ spec/models/change_request_spec.rb
@@ -5,10 +5,10 @@ require 'rails_helper'
 RSpec.describe ChangeRequest, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:change_title) }
-    it { should validate_presence_of(:application_pages) }
-    it { should validate_presence_of(:number_roles) }
-    it { should validate_presence_of(:authentication_needed) }
-    it { should validate_presence_of(:custom_error_pages) }
+    it { should_not validate_presence_of(:application_pages) }
+    it { should_not validate_presence_of(:number_roles) }
+    it { should_not validate_presence_of(:authentication_needed) }
+    it { should_not validate_presence_of(:custom_error_pages) }
   end
 
   describe 'associations' do
