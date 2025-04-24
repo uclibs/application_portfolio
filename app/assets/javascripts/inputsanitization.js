c052a0a32646bf224da5ad911d6de1d98b8c9561
Thomas Scherz <scherztc@ucmail.uc.edu>
2025-04-24 16:25:36 -0400

    Fixes DOM loading inputsanitization error.

--- app/assets/javascripts/inputsanitization.js
+++ app/assets/javascripts/inputsanitization.js
@@ -1,4 +1,9 @@
-var createdbyfield = document.getElementsByClassName('regex-createdby')[0];
-createdbyfield.onkeyup = function() {
-    createdbyfield.value = createdbyfield.value.replace(/[^a-zA-Z0-9 ]/, '');
-}
+document.addEventListener("DOMContentLoaded", function () {
+  var createdbyfield = document.getElementsByClassName('regex-createdby')[0];
+  if (createdbyfield) {
+    createdbyfield.onkeyup = function() {
+      createdbyfield.value = createdbyfield.value.replace(/[^a-zA-Z0-9 ]/g, ''); // fixed regex too
+    };
+  }
+});
+
