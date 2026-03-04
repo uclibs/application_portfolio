d6824a19370693a88f8183fa8c9d462e153d1aee
Janell Huyck <huyckjl@ucmail.uc.edu>
2026-03-04 14:28:21 -0500

    Rubocop fix

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_04_14_164358) do
+ActiveRecord::Schema[8.1].define(version: 20_250_414_164_358) do
   create_table "change_requests", force: :cascade do |t|
     t.integer "application_pages"
     t.boolean "authentication_needed", default: true
